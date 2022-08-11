25eb4899c263b57326d06caed2da03c11fed3a4a
Kravtsov <kravtsovofficial99@gmail.com>
2022-08-11 20:07:37 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -6,24 +6,34 @@ const input = document.querySelector("#search-box")
 const list = document.querySelector(".country-list");
 
 
-
-fetch("https://restcountries.com/v3/all?fields=name,capital,population,flags,languages ")
-    .then(response => response.json())
-    .then((date) => {insertContent(date)})
+  fetchCountries()
+    .then(insertContent)
     .catch((error) => { console.log(error) })
 
+function fetchCountries() {
+  return fetch(`https://restcountries.com/v3.1/name/{name}?fullText=true`)
+    .then(response => {return response.json()})
+};
 
     const createListItem = (item) => `<li>
-  <img src="${item.flags[1]}" alt="">
-  <h2>${item.name.official}</h2>
-  <p>${item.capital}</p>
-  <p>${item.population}</p>
-  <p>${item.languages}</p>
+  <img src="${item.flags[1]}" alt="${item.name.official}">
+  <h2>${item.name.official ? item.name.official : ""}</h2>
+  <p>${item.capital ? item.capital : ""}</p>
+  <p>${item.population ? item.population : ""}</p>
+  <p>${Object.values(item.languages) ? Object.values(item.languages) : ""}</p>
 </li>`;
 
  const generateContent = (array) => array?.reduce((acc, item) => acc + createListItem(item), "");
 
-const insertContent = (array) => {
+function insertContent (array) {
   const result = generateContent(array);
   list.insertAdjacentHTML("beforeend", result);
-};
+}
+
+
+// fetch("https://restcountries.com/v3/all?fields=name,capital,population,flags,languages ")
+//     .then(response => response.json())
+//     .then((date) => {insertContent(date)})
+//   fetchCountries()
+//     .then(insertContent)
+//     .catch((error) => { console.log(error) })
